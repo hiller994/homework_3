15408e54215dd114f73913b515eaf2653927639d
Andrey <hiller994@gmail.com>
2025-02-06 22:43:05 +0300

    Создан файл "conftest.py". Добавлена фикстуара на изменения размера браузера

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selene import browser, be, have
+
+@pytest.fixture(scope='session')
+def open_browser():
+    browser.config.window_height = 480  # высота браузера
+    browser.config.window_width = 640  # ширина браузера
+    yield
+    browser.quit()
