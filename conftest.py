917c9a7ae5faa9fb5d72f4ae6cbfd2b1d16c72eb
Andrey <hiller994@gmail.com>
2025-02-06 22:54:14 +0300

    перенес открытие браузера в фикстуру, убрал открытие браузера из теста

--- conftest.py
+++ conftest.py
@@ -5,5 +5,6 @@ from selene import browser, be, have
 def open_browser():
     browser.config.window_height = 480  # высота браузера
     browser.config.window_width = 640  # ширина браузера
+    browser.open('https://duckduckgo.com/')
     yield
     browser.quit()
